9e2d5999c2b5b45881ce32fd1b79292519611dd0
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2024-03-09 15:17:59 +0000

    implented transform_to_list, dot_product, is_valid_id_number #2

--- app/functions.py
+++ app/functions.py
@@ -49,3 +49,56 @@ def is_odd(number: int) -> bool:
                         Oddness is only defined for integers.")
 
     return number % 2 != 0
+
+def transform_to_list(id_number: int) -> list:
+    """
+    This function transforms the input number into a list of integers
+    """
+
+    return [int(digit) for digit in str(id_number)]
+
+def dot_product(a: int, b: int) -> int:
+    """
+    This function implements the element-wise multiplication of two lists.
+    We implemented this function only to keep the number of dependeciess low.
+    However, if high performance is needed, consider using numpy.
+
+    :param a: a list of numbers
+    :param b: a list of numbers
+    :return: the dot product of a and b
+    """
+
+    if len(a) != len(b):
+        raise Exception("The lists must have the same length!")
+
+    return sum([a[i] * b[i] for i in range(len(a))])
+
+def is_valid_id_number(id_number: int) -> bool:
+    """
+    This function checks if the input is a valid Norwegian ID number.
+    We will use conditionally defined return calls, which can speed up
+    the function avoiding unnecessary checks.
+    :param id_number: 11 digits Norwegian id number
+    :return: True if the number is valid, False otherwise
+    """
+    control_vector1 = [3, 7, 6, 1, 8, 9, 4, 5, 2, 1]
+    control_vector2 = [5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 1]
+
+    if type(id_number) != int:
+        return False
+    else:
+        id_number_list = transform_to_list(id_number)
+
+    if len(id_number_list) != 11:
+        return False
+    else:
+        control_number_1 = dot_product(id_number_list[:10], control_vector1)
+        control_number_2 = dot_product(id_number_list, control_vector2)
+
+    if control_number_1 % 11 != 0:
+        return False
+    elif control_number_2 % 11 != 0:  
+        return False
+    else:
+        return True 
+
