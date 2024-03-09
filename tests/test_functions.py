1b7b9514fd5392df32ad4c623c1d6568a314bddc
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2024-03-09 21:46:24 +0000

    add more tests for is_valid_id_number

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,6 +2,7 @@ import unittest
 
 from app.functions import (split, 
                            is_odd, 
+                           get_gender_from,
                            get_age_from, 
                            transform_to_list,
                            dot_product,
@@ -31,6 +32,10 @@ class TestFunctions(unittest.TestCase):
         self.assertRaises(TypeError, is_odd, '3')
         self.assertRaises(TypeError, is_odd, 3.0)
 
+    def test_get_gender_from(self):
+        self.assertEqual(get_gender_from('30108934489'), "female")
+        self.assertEqual(get_gender_from('30108934389'), "male")
+
     def test_get_age_from(self):
         self.assertEqual(get_age_from('30108938901'), 34)
         self.assertRaises(Exception, get_age_from, '30108998901')
@@ -46,6 +51,8 @@ class TestFunctions(unittest.TestCase):
         self.assertTrue(is_valid_id_number('26018835419'))
         self.assertFalse(is_valid_id_number('30108938901'))
         self.assertTrue(is_valid_id_number('02068503429'))
+        self.assertFalse(is_valid_id_number('Madalena Morgado'))
+        self.assertFalse(is_valid_id_number('1234'))
 
 if __name__ == '__main__':
     unittest.main()
