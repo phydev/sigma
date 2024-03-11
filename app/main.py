45a72620a902820a3c57a525412bbbcbc6009b8c
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2024-03-11 14:56:55 +0000

    change 127.0.0.1 to 0.0.0.0 because of docker

--- app/main.py
+++ app/main.py
@@ -143,4 +143,4 @@ if __name__ =='__main__':
     # run rest api with uvicorn
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8070)
+    uvicorn.run(app, host="0.0.0.0", port=8070)
